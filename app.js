2f990ae5b02b7f2d864f548bb881a13de542c79e
Artyom <narshinovartyom@mail.ru>
2021-12-16 21:38:12 +0400

    ready to use

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const httpExample = {
             xhr.open('POST', url)
             xhr.addEventListener('load', () => {
                 if ((Math.round(Math.floor(xhr.status) / 100)) != 2) {
-                    console.log(`Error. Status of request is ${xhr.status}`)
+                    alert('Please select contry or use the query')
                 }
 
                 const response = xhr.responseText
@@ -48,8 +48,11 @@ const httpExample = {
 
 
 const http = httpExample
+const newsContainer = document.querySelector('.row')
 
 const select = document.querySelector('.form-select')
+const inputQuery = document.querySelector('.form-control')
+
 const btn = document.querySelector('.btn')
 
 const newsService = (() => {
@@ -61,57 +64,81 @@ const newsService = (() => {
             http.get(`${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`, cb)
         },
         everything(query, cb) {
-            http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb)
+            if (query) {
+                http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb)
+            }
         }
     }
 })
 
 document.addEventListener('DOMContentLoaded', () => {
+    loadQueries()
     loadCountryNews()
+
+    // for (item of newsContainer.children) {
+    //     newsContainer.removeChild(item)
+    // }
 })
 
 const loadCountryNews = () => {
-    let country = ''
-    btn.addEventListener('click', () => {
-        country = select.value
-        loadNews()
-    })
+    if (inputQuery.value == '') {
+        let country = ''
+        btn.addEventListener('click', () => {
+            country = select.value
+            loadNews()
+        })
 
-    
-    // Load news
-    const loadNews = () => {
-        newsService().topHeadlines(country, onGetResponse)
+
+        // Load news
+        const loadNews = () => {
+            newsService().topHeadlines(country, onGetResponse)
+        }
     }
 }
 
+const loadQueries = () => {
+    let query = ''
+    btn.addEventListener('click', () => {
+        query = inputQuery.value
+        loadQuery()
+    })
+
+    const loadQuery = () => {
+        newsService().everything(query, onGetResponse)
+    }
+
+
+}
 
 const onGetResponse = (res) => {
-    console.log(res)
     renderNews(res.articles)
 }
 
 // Render news
 const renderNews = (news) => {
-    const newsContainer = document.querySelector('.row')
-    let fragment = ''
-    news.forEach((item) => {
-        const el = newsTemplate(item)
-        fragment += el
-    })
-    newsContainer.insertAdjacentHTML('afterbegin', fragment)
+    if (news) {
+        let fragment = ''
+        news.forEach((item) => {
+            if (item) {
+                const el = newsTemplate(item)
+                fragment += el
+            }
+        })
+        newsContainer.insertAdjacentHTML('afterbegin', fragment)
+    }
 }
 
 // News item template function
 
-const newsTemplate = ({ content, description, urlToImage, url }) => {
+const newsTemplate = ({ title, description, urlToImage, url, publishedAt}) => {
     return `
         <div class="card mb-3 mt-3">
             <img src="${urlToImage}" class="card-img-top p-3" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${description || null}</h5>
-                <p class="card-text">${content || null}</</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                <a href="${url}" class="btn btn-primary">Article link</a>
+                <h5 class="card-title">${title || null}</h5>
+                <p class="card-text">${description || null}</</p>
+                <p class="card-text"><small class="text-muted">${publishedAt}</small></p>
+                <a href="${url || null}" class="btn btn-primary">Article link</a>
             </div>
         </div>
     `
